cc2aff3e2b60d87f8c06b5dea833e194c6d6c430
zubow <zubow@tkn.tu-berlin.de>
2023-12-15 11:53:31 +0100

    flash of AP

--- search_ap.sh
+++ search_ap.sh
@@ -2,13 +2,15 @@
 
 ap_mac_addr="48:27:e2:3b:33:2d"
 
+# store the TTY ID in file
+echo "" > ap_tty.dat
+
 for dev in /dev/ttyACM*; do
-	#echo "checking $dev"
 	dev_mac_addr=$(udevadm info -a -p  $(udevadm info -q path -n $dev) | grep ATTRS{serial} | head -n1 | awk -F "==" '{ print $2}' | sed -s "s/\"//g")
-	#echo "$dev -> $dev_mac_addr"
 
 	if [[ ${ap_mac_addr} =~ ${dev_mac_addr,,} ]]
 	then
   		echo "AP found: $dev -> $dev_mac_addr"
+  		echo "${dev/\/dev\/ttyACM/}" >> ap_tty.dat
 	fi
 done
